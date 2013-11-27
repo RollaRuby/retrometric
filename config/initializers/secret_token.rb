1295101784d0e97db5f0a8faf57db633af848d99
Todd Pickell <myappleguy@gmail.com>
2013-11-26 20:02:13 -0600

    initial commit of rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Retrometric::Application.config.secret_key_base = 'a75a92cac3c9da3bba1543f920322375891a09a4ce95d8ec7c89409d415fabc6bf5b6628a7f52788ffb235574d59bbc5753be107b511a41c0c5f82871fe1af19'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Retrometric::Application.config.session_store :cookie_store, key: '_retrometric_session'
